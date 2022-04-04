d897170de555c7ca389b66696fb10d158b0e99f3
mwiecaszekUG <qodspl@gmail.com>
2022-04-04 09:35:08 +0200

    2 juz dziala

--- Zadania/src/lab_04/zad_02.py
+++ Zadania/src/lab_04/zad_02.py
@@ -5,11 +5,11 @@ from pyswarms.utils.plotters.formatters import Mesher
 # Run optimizer
 options = {'c1':0.5, 'c2':0.3, 'w':0.9}
 optimizer = ps.single.GlobalBestPSO(n_particles=10, dimensions=2, options=options)
-# historia kosztów i pozycji
-cost_history, pos_history = optimizer.optimize(fx.sphere, iters=50)
-print(cost_history)
-print(pos_history)
-#tworzenie animacji
-m = Mesher(func=fx.sphere)
-animation = plot_contour(pos_history=pos_history, mesher=m, mark=(0, 0))
-animation.save('plot0.gif', writer='imagemagick', fps=10)
+optimizer.optimize(fx.sphere, iters=50)
+# tworzenie animacji
+# m = Mesher(func=fx.sphere)
+m = Mesher(func=fx.crossintray)
+animation = plot_contour(pos_history=optimizer.pos_history,
+ mesher=m,
+mark=(0, 0))
+animation.save('fx.crossintrtay.gif', writer='imagemagick', fps=10)
